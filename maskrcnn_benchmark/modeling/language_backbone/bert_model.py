f3aef919c4a3781200097c947aaea3696a226e82
nobu-g <ueda-n127@hotmail.co.jp>
2023-10-23 09:13:33 +0000

    refactor language encoder

--- maskrcnn_benchmark/modeling/language_backbone/bert_model.py
+++ maskrcnn_benchmark/modeling/language_backbone/bert_model.py
@@ -4,49 +4,33 @@ import numpy as np
 import torch
 from torch import nn
 
-# from pytorch_pretrained_bert.modeling import BertModel
-from transformers import AutoConfig, AutoModel, BertConfig, BertModel, RobertaConfig, RobertaModel
+from transformers import AutoConfig, AutoModel
 
 
 class BertEncoder(nn.Module):
     def __init__(self, cfg):
         super().__init__()
         self.cfg = cfg
-        self.bert_name = cfg.MODEL.LANGUAGE_BACKBONE.MODEL_TYPE
+        self.model_type = cfg.MODEL.LANGUAGE_BACKBONE.MODEL_TYPE
         print(
             "LANGUAGE BACKBONE USE GRADIENT CHECKPOINTING: ",
             self.cfg.MODEL.LANGUAGE_BACKBONE.USE_CHECKPOINT,
         )
 
-        if self.bert_name == "bert-base-uncased":
-            config = BertConfig.from_pretrained(self.bert_name)
-            config.gradient_checkpointing = self.cfg.MODEL.LANGUAGE_BACKBONE.USE_CHECKPOINT
-            self.model = BertModel.from_pretrained(self.bert_name, add_pooling_layer=False, config=config)
-            self.language_dim = 768
-        elif self.bert_name == "roberta-base":
-            config = RobertaConfig.from_pretrained(self.bert_name)
-            config.gradient_checkpointing = self.cfg.MODEL.LANGUAGE_BACKBONE.USE_CHECKPOINT
-            self.model = RobertaModel.from_pretrained(self.bert_name, add_pooling_layer=False, config=config)
-            self.language_dim = 768
-        elif self.bert_name == "xlm-roberta-base":
-            config = AutoConfig.from_pretrained(self.bert_name)
-            config.gradient_checkpointing = self.cfg.MODEL.LANGUAGE_BACKBONE.USE_CHECKPOINT
-            self.model = AutoModel.from_pretrained(self.bert_name, add_pooling_layer=False, config=config)
-            self.language_dim = 768
-        else:
-            raise NotImplementedError
-
+        config = AutoConfig.from_pretrained(self.model_type)
+        config.gradient_checkpointing = self.cfg.MODEL.LANGUAGE_BACKBONE.USE_CHECKPOINT
+        self.model = AutoModel.from_pretrained(self.bert_name, add_pooling_layer=False, config=config)
         self.num_layers = cfg.MODEL.LANGUAGE_BACKBONE.N_LAYERS
 
     def forward(self, x):
-        input = x["input_ids"]
-        mask = x["attention_mask"]
+        input_ids = x["input_ids"]
+        attention_mask = x["attention_mask"]
 
         if self.cfg.MODEL.DYHEAD.FUSE_CONFIG.USE_DOT_PRODUCT_TOKEN_LOSS:
             # with padding, always 256
             outputs = self.model(
-                input_ids=input,
-                attention_mask=mask,
+                input_ids=input_ids,
+                attention_mask=attention_mask,
                 output_hidden_states=True,
             )
             # outputs has 13 layers, 1 input layer and 12 hidden layers
@@ -57,15 +41,15 @@ class BertEncoder(nn.Module):
             # language embedding has shape [len(phrase), seq_len, language_dim]
             features = features / self.num_layers
 
-            embedded = features * mask.unsqueeze(-1).float()
-            aggregate = embedded.sum(1) / (mask.sum(-1).unsqueeze(-1).float())
+            embedded = features * attention_mask.unsqueeze(-1).float()
+            aggregate = embedded.sum(1) / (attention_mask.sum(-1).unsqueeze(-1).float())
 
         else:
             # without padding, only consider positive_tokens
-            max_len = (input != 0).sum(1).max().item()
+            max_len = (input_ids != 0).sum(1).max().item()
             outputs = self.model(
-                input_ids=input[:, :max_len],
-                attention_mask=mask[:, :max_len],
+                input_ids=input_ids[:, :max_len],
+                attention_mask=attention_mask[:, :max_len],
                 output_hidden_states=True,
             )
             # outputs has 13 layers, 1 input layer and 12 hidden layers
@@ -76,13 +60,13 @@ class BertEncoder(nn.Module):
             # language embedding has shape [len(phrase), seq_len, language_dim]
             features = features / self.num_layers
 
-            embedded = features * mask[:, :max_len].unsqueeze(-1).float()
-            aggregate = embedded.sum(1) / (mask.sum(-1).unsqueeze(-1).float())
+            embedded = features * attention_mask[:, :max_len].unsqueeze(-1).float()
+            aggregate = embedded.sum(1) / (attention_mask.sum(-1).unsqueeze(-1).float())
 
         ret = {
             "aggregate": aggregate,
             "embedded": embedded,
-            "masks": mask,
+            "masks": attention_mask,
             "hidden": encoded_layers[-1],
         }
         return ret

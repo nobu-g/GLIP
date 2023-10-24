59d35cd6af53a3d923e4ae69859c1ddede7d2b4b
nobu-g <ueda-n127@hotmail.co.jp>
2023-10-25 00:19:20 +0900

    fix fine-tune_flickr_ja.sh

--- fine-tune_flickr_ja.sh
+++ fine-tune_flickr_ja.sh
@@ -3,12 +3,17 @@
 set -euo pipefail
 set -x
 
-poetry run python -m torch.distributed.launch --nproc_per_node=4 tools/finetune.py \
+GLOBAL_BATCH_SIZE=36
+DEVICES=3
+MAX_EPOCH=4
+EXPR_NAME="pretrained_roberta_flickr_ja_mixed_${MAX_EPOCH}e_b${GLOBAL_BATCH_SIZE}"
+
+poetry run python -m torch.distributed.launch --nproc_per_node="${DEVICES}" tools/finetune.py \
   --config-file ./configs/finetune/glip_Swin_T_O365_GoldG_flickr-ja.yaml \
   --skip-test \
   --use-tensorboard \
   --evaluate_only_best_on_test \
-  MODEL.BACKBONE.FREEZE_CONV_BODY_AT 2 \
+  OUTPUT_DIR "./OUTPUT/${EXPR_NAME}" \
   MODEL.DYHEAD.USE_CHECKPOINT True \
   TEST.DURING_TRAINING False \
   TEST.IMS_PER_BATCH 8 \
@@ -16,10 +21,10 @@ poetry run python -m torch.distributed.launch --nproc_per_node=4 tools/finetune.
   DATASETS.USE_OVERRIDE_CATEGORY True \
   DATASETS.SHUFFLE_SEED 3 \
   DATASETS.USE_CAPTION_PROMPT True \
-  SOLVER.MAX_EPOCH 1 \
+  SOLVER.MAX_EPOCH "${MAX_EPOCH}" \
   SOLVER.WARMUP_ITERS 100 \
   SOLVER.USE_AMP True \
-  SOLVER.IMS_PER_BATCH 16 \
+  SOLVER.IMS_PER_BATCH "${GLOBAL_BATCH_SIZE}" \
   SOLVER.WEIGHT_DECAY 0.05 \
   SOLVER.FIND_UNUSED_PARAMETERS False \
   SOLVER.TEST_WITH_INFERENCE False \
@@ -28,5 +33,4 @@ poetry run python -m torch.distributed.launch --nproc_per_node=4 tools/finetune.
   SOLVER.STEP_PATIENCE 3 \
   SOLVER.CHECKPOINT_PER_EPOCH 1.0 \
   SOLVER.AUTO_TERMINATE_PATIENCE 8 \
-  SOLVER.MODEL_EMA 0.0 \
-  SOLVER.TUNING_HIGHLEVEL_OVERRIDE full
+  SOLVER.MODEL_EMA 0.0

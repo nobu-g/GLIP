331bae8b3998c251fac8cb256295fca7c2f2efcb
nobu-g <ueda-n127@hotmail.co.jp>
2023-11-24 17:24:47 +0900

    fix fine-tune_debug.sh

--- fine-tune_debug.sh
+++ fine-tune_debug.sh
@@ -3,10 +3,11 @@
 set -euo pipefail
 set -x
 
-readonly GLOBAL_BATCH_SIZE="${GLOBAL_BATCH_SIZE:-"3"}"
+readonly GLOBAL_BATCH_SIZE="${GLOBAL_BATCH_SIZE:-"4"}"
+readonly DEVICES="${DEVICES:-"1"}"
 readonly EXPR_NAME="pretrained_roberta_flickr_ja_mixed_debug"
 
-# poetry run python -m torch.distributed.launch --nproc_per_node=2 tools/finetune.py \
+# poetry run python -m torch.distributed.launch --nproc_per_node="${DEVICES}" tools/finetune.py \
 poetry run python -m ipdb -c continue tools/finetune.py \
   --config-file ./configs/finetune/flickr_ja_mixed.yaml \
   --skip-test \
@@ -15,8 +16,10 @@ poetry run python -m ipdb -c continue tools/finetune.py \
   --skip_loading_text_encoder \
   OUTPUT_DIR "./OUTPUT/${EXPR_NAME}" \
   MODEL.DYHEAD.USE_CHECKPOINT True \
-  TEST.DURING_TRAINING True \
-  TEST.IMS_PER_BATCH 3 \
+  MODEL.LANGUAGE_BACKBONE.USE_CHECKPOINT True \
+  MODEL.BACKBONE.USE_CHECKPOINT True \
+  TEST.DURING_TRAINING False \
+  TEST.IMS_PER_BATCH 4 \
   TEST.EVAL_TASK detection \
   DATASETS.USE_OVERRIDE_CATEGORY True \
   DATASETS.SHUFFLE_SEED 3 \
